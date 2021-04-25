321013d7b2ee3e9d0d6e9dbaf74c5ba4b958725a
Asia-Siddiqui <muhmmadfawadsiddiqui894@gmail.com>
2021-04-25 13:49:25 +0500

    few lines added

--- Project.r
+++ Project.r
@@ -9,3 +9,11 @@ my_turtle = turtle.Turtle()
 for i in range(18):
     my_turtle.circle(100)
     my_turtle.right(20)
+
+    i = 2
+    sum=0
+while(i<=n)
+      sum=sum+i
+    i=i+2
+    print("sum of even number=",sum)
+    
